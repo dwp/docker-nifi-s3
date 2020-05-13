4d934bd0761454b38373e551732d3a5434f984da
mike-iw <mike.winker@infinityworks.com>
2020-05-13 13:40:40 +0100

    Add Dockerfile and java program for importing S3 config for Apache Nifi

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM apache/nifi:1.9.0
+COPY docker-nifi-s3.jar .
+
+ENTRYPOINT []
---
+++ src/main/java/uk/gov/dwp/dataworks/GetTemplateFromS3.java
@@ -0,0 +1,45 @@
+package uk.gov.dwp.dataworks;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.SdkClientException;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+
+public class GetTemplateFromS3 {
+
+    private static final String DESTINATION = "/conf/flow.xml.gz";
+
+    public static void main(final String[] args) throws IOException {
+        try {
+
+            // these will be
+            final String clientRegion = args[1];
+            final String bucketName = args[2];
+            final String sourceKey = args[3];
+
+            final AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                    .withCredentials(new ProfileCredentialsProvider())
+                    .withRegion(Regions.valueOf(clientRegion))
+                    .build();
+
+            final GetObjectRequest request = new GetObjectRequest(bucketName, sourceKey);
+            final S3Object s3Object = s3Client.getObject(request);
+
+            final S3ObjectInputStream inputStream = s3Object.getObjectContent();
+            FileUtils.copyInputStreamToFile(inputStream, new File(DESTINATION));
+        } catch (final AmazonServiceException e) {
+            e.printStackTrace();
+        } catch (final SdkClientException e) {
+            e.printStackTrace();
+        }
+    }
+}
